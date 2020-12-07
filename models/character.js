37115b7ec7e2e2ae94ba92a381a6c8d57a19f1e5
marcosmarangoni <marcosmarangoni2@gmail.com>
2020-12-06 19:38:56 -0800

    First commit

--- models/character.js
+++ models/character.js
@@ -1,12 +1,6 @@
-//med.js
-
-//mongoose requirements
 const mongoose = require('mongoose');
-
-//create mongoose schema object
 const Schema = mongoose.Schema;
 
-//create schema for our collection (meds_col)
 const CharacterSchema = new Schema({
   name: {
     type: String,
@@ -20,15 +14,9 @@ const CharacterSchema = new Schema({
     type: String,
     trim: true,
   }
+});
   
-//}, {collection : 'meds_col'});
-	});
-//reference existing collection
 const col = "characters";
-//const Med = mongoose.model("Med", MedSchema);
-//prepare model
 const Character = mongoose.model("Character", CharacterSchema, col);
-
-//export model
 module.exports = Character;
 
